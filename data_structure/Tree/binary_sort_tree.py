190c409992a3c5979ee770479d02a235f4fb9b68
wangcx <tingfen1635@163.com>
2022-11-28 11:35:58 +0800

    feat:binary search tree delete

--- data_structure/Tree/binary_sort_tree.py
+++ data_structure/Tree/binary_sort_tree.py
@@ -67,7 +67,55 @@ class BST:
 
         return None
 
+    def delete(self, val):
+        """
+        假设要删除的节点q、 其父节点为p
+            *** 如果被删节点是根结点， 需要做特殊处理
+            1 - 通过val找到节点p、父节点q
+            2 - 删除节点，不同的情形
+                - 如果q是叶子节点， 则将p对q的引用指向为None
+                - 如果q不是叶子节点， 需要将q的子树链接到删除q之后的树中
+                    - 如果 q 没有左子树，将q的右子树改为p的左子树
+                    - 如果 q 有左子树， 需要找到q左子树的最右节点 r （左子树中的最大值）
+                        - 用q的左子节点作为p的左子节点
+                        - 用q的右子树作为r的右子树
+        """
+        p, q = None, self.root
+
+        # 删除节点为q， 父节点q
+        while q and q.val != val:
+            p = q
+            if val < q.val:
+                q = q.left
+            else:
+                q = q.right
+
+        if q is None:
+            return
+
+        if q.left is None:
+            if p is None:  # q是根节点
+                self.root = q.right
+            elif q is p.left:
+                p.left = q.right
+            else:
+                p.right = q.right
+            return
+
+        r = q.left
+        while r.right:
+            r = r.right
+
+        r.right = q.right
+        if p is None:
+            self.root = q.left  # q是根结点
+
+        elif p.left is q:
+            p.left = q.left
+        else:
+            p.right = q.left
+
 
 if __name__ == '__main__':
-    tree = BST([3, 4, 9, 13, 8, 7, 9, 15])
+    tree = BST([10, 3, 4, 9, 13, 8, 7, 9, 15])
     in_order(tree.root)
