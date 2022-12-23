422664e516d25a167b38384b04bc82436962f91c
wangcx <tingfen1635@163.com>
2022-12-23 11:53:20 +0800

    feat: dp

--- algorithm/动态规划/4.最长公共子序列.py
+++ algorithm/动态规划/4.最长公共子序列.py
@@ -20,3 +20,58 @@
 
      c[i,j] 表示Xi和Yj的LCS长度
 """
+
+
+def lcs_length(x, y):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:
+                c[i][j] = c[i - 1][j - 1] + 1
+            else:
+                c[i][j] = max(c[i - 1][j], c[i][j - 1])
+    return c[m][n]
+
+
+def lcs(x, y):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    b = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:  # 位置上的字符匹配的时候，来自左上方
+                c[i][j] = c[i - 1][j - 1] + 1
+                b[i][j] = 1
+            elif c[i - 1][j] > c[i][j - 1]:  # 来自上方
+                c[i][j] = c[i - 1][j]
+                b[i][j] = 2
+            else:  # 来自左方
+                c[i][j] = c[i][j - 1]
+                b[i][j] = 3
+
+    return c[m][n], b
+
+
+def lcs_traceback(x, y):
+    c, b = lcs(x, y)
+    i = len(x)
+    j = len(y)
+    res = []
+    while i > 0 and j > 0:
+        if b[i][j] == 1:  # 来自左上方
+            res.append(x[i - 1])
+            i -= 1
+            j -= 1
+        elif b[i][j] == 2:  # 来自上方
+            i -= 1
+        else:
+            j -= 1
+    return "".join(reversed(res))
+
+
+print(lcs_traceback("ABCBDAB", "BDCABA"))
