f996f2959939acbf06d16bcd8832e0f990def987
wangcx <tingfen1635@163.com>
2023-01-04 12:17:16 +0800

    feat: diff

--- data_structure/List/304.二维区域和检索 - 矩阵不可变.py
+++ data_structure/List/304.二维区域和检索 - 矩阵不可变.py
@@ -4,7 +4,9 @@
     计算其子矩形范围内元素的总和，该子矩阵的 左上角 为 (row1, col1) ，右下角 为 (row2, col2) 。
 实现 NumMatrix 类：
     NumMatrix(int[][] matrix) 给定整数矩阵 matrix 进行初始化
-    int sumRegion(int row1,int col1,int row2,int col2) 返回左上角 (row1,col1)、右下角 (row2,col2)所描述的子矩阵的元素总和 。
+    int sumRegion(int row1,int col1,int row2,int col2) 返回左上角 (row1,col1)、右下角 (row2,col2)所描述的子矩阵的元素总和。
+
+注意：前缀和主要适用的场景是原始数组不会被修改的情况下，频繁查询某个区间的累加和。
 """
 
 
---
+++ data_structure/List/370.区间加法.py
@@ -0,0 +1,75 @@
+"""
+题目：
+    假设你有一个长度为n的数组，初使情况下所有的数字均为0，你将会被给出k个更新操作，
+    其中每个操作会被表示为一个三元组，[startIndex, endIndex, inc], 你需要将子数组A[startIndex....endIndex]
+    包括（startIndex 和 endIndex）
+    请返回k次操作后的数组
+
+分析：
+    TODO：技巧 -> 差分数组的主要适用场景是频繁对原始数组的某个区间的元素进行增减
+    1 - 将nums数组构造一个diff差分数组，diff[i] 就是nums[i] 和 nums[i - 1]之差
+        diff = [0 for _ in range(len(nums))]
+        diff[0] = nums[0]
+        for i in range(1, len(nums)):
+            diff [i] = nums[i] - nums[i - 1]
+
+    2 - 通过差分数组可以推出原始数组代码
+        res = []
+        res[0] = diff[0]
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] + diff[i]
+
+
+    3 - 通过差分数组进行快速的增减操作
+        如果想对区间nums[i...j] (包含i and j)  的元素全部加3，那么只需要将diff[i] += 3,然后再让diff[j+1] -=3
+"""
+
+
+class Difference(object):
+
+    def __init__(self, nums):
+        self.diff = self.diffList(nums)
+
+    @staticmethod
+    def diffList(nums):
+        """
+        构造差分数组
+        """
+        diff = [0 for _ in range(len(nums))]
+        diff[0] = nums[0]
+        for i in range(1, len(nums)):
+            diff[i] = nums[i] - nums[i - 1]
+        return diff
+
+    def result(self):
+        """
+        获取操作后的数组
+        """
+        res = []
+        res[0] = self.diff[0]
+        for i in range(1, len(self.diff)):
+            res[i] = res[i - 1] + self.diff[i]
+        return res
+
+    def increment(self, i, j, inc):
+        """
+        对数组某一个区间的数进行增减（包含边界值）
+        """
+        self.diff[i] += inc
+        if j + 1 <= len(self.diff):
+            self.diff[j] -= inc
+
+
+def getModifiedArray(length, updates):
+    """
+    :param length: 初始化一个长度为5的数组
+    :param updates:对指定区间进行对应的操作
+    :return: 返回操作后的数组
+    """
+    df = Difference([0 for _ in range(5)])
+    for u in updates:
+        df.increment(u[0], u[1], u[2])
+    return df.result()
+
+
+
