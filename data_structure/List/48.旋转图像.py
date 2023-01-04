4795de0efb93c4b9394488061bca938a85b27389
wangcx <tingfen1635@163.com>
2023-01-04 18:02:33 +0800

    feat: matrix

--- data_structure/List/48.旋转图像.py
+++ data_structure/List/48.旋转图像.py
@@ -13,5 +13,30 @@
 分析：
     如何「原地」旋转二维矩阵？稍想一下,感觉操作起来非常复杂,可能要设置巧妙的算法机制来「一圈一圈」旋转矩阵
 
-
+    常规思路：
+        寻找原始坐标和旋转坐标的映射规律，但我们时候可以让思维跳跃，尝试将矩阵进行反转，镜像对称操作
 """
+
+
+def rotate(matrix):
+    """
+    将二维矩阵matrix顺时针原地旋转90度
+    """
+    # 先将二维矩阵沿着左上右下的对角线做镜像对称
+    n = len(matrix)  # 行，由于是N x N 的矩阵
+    for i in range(n):
+        for j in range(i, n):  # 遍历对角线的一般就可以
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for l in matrix:
+        reserve(l)
+
+
+def reserve(l):
+    left = 0
+    right = len(l) - 1
+    while left < right:
+        l[left], l[right] = l[right], l[left]
+        left += 1
+        right -= 1
+    return l
