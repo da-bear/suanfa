b03a9972525aa496658c7e26470425e35628cb09
wangcx <tingfen1635@163.com>
2023-03-04 14:20:44 +0800

    feat

--- base_file/cal_time.py
+++ base_file/cal_time.py
@@ -13,3 +13,20 @@ def timeit(func):
         print("%s func running %s sec" % (func.__name__, (t2 - t1)))
         return result
     return wrapper
+
+
+# def branch_ft():
+#     # test 键盘
+#     #xisdlufngsi
+#     #leiofeiAPi
+#     # 增加比价操作11
+#     # 往场次哦那个
+#     # 纵有疾风起。人生不言弃
+
+
+def test():
+    """ jianopan s
+    """
+    a = "dad j  ffs a "
+    b = max(3, 8)
+    return b
